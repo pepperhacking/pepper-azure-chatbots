17e8020e9e631fd78bf450496f52f98483073f99
Emile KROEGER <ekroeger@softbankrobotics.com>
2018-09-11 13:49:41 +0800

    Implement proper RobotActivity

--- app/src/main/java/com/softbankrobotics/chatbotdemo/MainActivity.kt
+++ app/src/main/java/com/softbankrobotics/chatbotdemo/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.softbankrobotics.chatbotdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        QiSDK.register(this, this)
+    }
+
+    override fun onRobotFocusGained(qiContext: QiContext) {
+    }
+
+    override fun onRobotFocusLost() {
+    }
+
+    override fun onRobotFocusRefused(reason: String) {
     }
 }
